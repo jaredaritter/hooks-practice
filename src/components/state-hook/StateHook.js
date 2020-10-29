e1db98b1cf1e39d5a53e193d6b154cd41f91e37f
jared <38332939+jaredaritter@users.noreply.github.com>
2020-10-29 17:08:54 -0400

    Add state-hook component

--- src/components/state-hook/StateHook.js
+++ src/components/state-hook/StateHook.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react';
 
 function StateHook() {
+  const [count, setCount] = useState(0);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
+
+  const todoList = todos.map((todo, i) => {
+    return <li key={i}>{todo.text}</li>;
+  });
+
+  const addTodo = () => {
+    const todo = prompt('Next todo?');
+    setTodos([...todos, { text: todo }]);
+  };
+
   return (
     <div>
       <h2>StateHook Component</h2>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Count</button>
+      <p>Fruit: {fruit}</p>
+      <button
+        onClick={() => setFruit(fruit === 'banana' ? 'orange' : 'banana')}
+      >
+        Fruit
+      </button>
+      <ul>{todoList}</ul>
+      <button onClick={addTodo}>Add Todo</button>
     </div>
   );
 }
